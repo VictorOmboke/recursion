6ece439561926644ca4fe2d1b93804f23da5b0c9
Victor Omboke <victoromboke@gmail.com>
2024-06-02 20:35:05 -0700

    Add iterative and recursive fibonacci functions

--- fib.js
+++ fib.js
@@ -1,2 +1,49 @@
 #!/usr/bin/env node
 
+function fibs(num) {
+  if (num <= 0) {
+    return [];
+  }
+
+  let array = [0];
+
+  if (num === 1) {
+    return array;
+  }
+
+  array.push(1);
+
+  for (let i = 2; i < num; i++) {
+    array.push(array[i - 1] + array[i - 2]);
+  }
+  return array;
+}
+
+console.log("Iterative solution for fibonacci(8):");
+console.log(fibs(8));
+
+console.log("Iterative solution for fibonacci(10):");
+console.log(fibs(10));
+
+function fibRec(num) {
+  if (num < 2) {
+    return num;
+  } else {
+    return fibRec(num - 1) + fibRec(num - 2);
+  }
+}
+
+function displayFibRec() {
+  let array = [];
+
+  let num = 10;
+
+  for (let i = 0; i < num; i++) {
+    array.push(fibRec(i));
+  }
+
+  return array;
+}
+
+console.log("Recursive solution for fibonacci(10):");
+console.log(displayFibRec());
